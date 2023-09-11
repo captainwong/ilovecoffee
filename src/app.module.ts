f64dd816fd1d57823975888ecbc757873c7bead3
i7-8700 <1281261856@qq.com>
2023-09-12 02:47:11 +0800

    docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'pass123',
+    database: 'postgres',
+    autoLoadEntities: true, // should not be used in production - it will load all entities at once
+    synchronize: true, // should not be used in production - it will drop the database
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
