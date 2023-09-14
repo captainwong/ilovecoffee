2a7a92ad23f38af5ced99fd2ad858f9457bd8ba7
i7-8700 <1281261856@qq.com>
2023-09-14 17:00:11 +0800

    add pointcuts with interceptors

---
+++ src/common/interceptors/wrap-response/wrap-response.interceptor.ts
@@ -0,0 +1,11 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class WrapResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log('Before...');
+    return next.handle().pipe(map(data => ({ data })));
+  }
+}
